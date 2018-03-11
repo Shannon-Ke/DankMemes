ce2f8def62293d9248b0d748e79a31619d20f98a
Choukri Nyon <CNyon52@gmail.com>
2018-03-10 20:44:17 -0500

    Add files via upload

---
+++ BUILDGT_DRONE.ino
@@ -0,0 +1,28 @@
+#define MOTOR_PIN 9
+#define INV_PIN 10
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(MOTOR_PIN, OUTPUT);
+pinMode(INV_PIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //Motor logic
+  analogWrite(MOTOR_PIN, 220);
+  digitalWrite(INV_PIN, LOW);
+  delay(2000);
+  
+  digitalWrite(MOTOR_PIN, LOW);
+  digitalWrite(INV_PIN, LOW);
+  delay(1000);
+
+  analogWrite(INV_PIN, 220);
+  digitalWrite(MOTOR_PIN, LOW);
+  delay(2000);
+
+  digitalWrite(MOTOR_PIN, LOW);
+  digitalWrite(INV_PIN, LOW);
+  delay(1000);
+}
